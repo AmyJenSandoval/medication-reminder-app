055d4ed57f4a9e76069180a20482c0c743261327
Amy Sandoval <amysandoval92@gmail.com>
2022-12-17 17:20:56 -0800

    Style Login page

--- src/modules/login/components/Login.js
+++ src/modules/login/components/Login.js
@@ -4,8 +4,35 @@ import styles from "../styles/Login.module.css";
 export const Login = () => {
   return (
     <div className={styles.container}>
-      <h1>Login</h1>
-      <p>This is the login page!</p>
+      <h1>Welcome to Mediminder</h1>
+      <form>
+        <input
+          aria-label="email"
+          name="email"
+          className={styles.input}
+          type="text"
+          placeholder="Email"
+          required
+        />
+
+        <input
+          aria-label="password"
+          name="password"
+          className={styles.input}
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <button className={styles.forgotPasswordButton}>
+          Forgot password?
+        </button>
+
+        <button className={styles.signUpButton} type="submit">
+          Sign Up
+        </button>
+
+        <button className={styles.logInButton}>Log In</button>
+      </form>
     </div>
   );
 };
