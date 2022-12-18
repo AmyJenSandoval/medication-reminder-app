7ca687738b17efab6d98ff11dad85b9decfe4cda
Amy Sandoval <amysandoval92@gmail.com>
2022-12-17 17:54:08 -0800

    Add image to Login

--- src/modules/login/components/Login.js
+++ src/modules/login/components/Login.js
@@ -4,7 +4,20 @@ import styles from "../styles/Login.module.css";
 export const Login = () => {
   return (
     <div className={styles.container}>
-      <h1>Welcome to Mediminder</h1>
+      <img
+        src="./images/kateryna-hliznitsova-Nqj2XWHy4K0-unsplash.jpg"
+        alt="cupped hands holding two red gel pills"
+        className={styles.img}
+      />
+
+      <div className={styles.heading}>
+        <h1>
+          <span>WELCOME TO</span>
+          <br></br>
+          Mediminder
+        </h1>
+      </div>
+
       <form>
         <input
           aria-label="email"
