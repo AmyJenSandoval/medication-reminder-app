2c89b381b236cb910bd232a45028f9ffcc64ab68
Amy Sandoval <amysandoval92@gmail.com>
2022-12-18 06:17:05 -0800

    Add MedicationCard to My Medications

--- src/modules/medication/components/Medication.js
+++ src/modules/medication/components/Medication.js
@@ -5,14 +5,29 @@ export const Medication = () => {
   return (
     <div className={styles.container}>
       <h1>Medication</h1>
-      <form className={styles.searchForm}>
+      <div className={styles.search}>
         <input
-          className={styles.search}
+          className={styles.input}
           placeholder="Search by medication"
           type="text"
         />
-        <button type="submit">Search</button>
-      </form>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Pradaxa</h1>
+          <p className={styles.body}>6:00AM</p>
+          <p className={styles.body}>1 capsule</p>
+          <button className={styles.button}>Edit</button>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Pradaxa</h1>
+          <p className={styles.body}>6:00PM</p>
+          <p className={styles.body}>1 capsule</p>
+          <button className={styles.button}>Edit</button>
+        </div>
+      </div>
     </div>
   );
 };
